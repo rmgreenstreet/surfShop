b070b7294b485f33ed03ac3e83cd5e6a521318cb
Robert Greenstreet <robertgreenstreet@gmail.com>
2020-01-08 12:27:39 -0600

    added comments to index controllers

--- controllers/index.js
+++ controllers/index.js
@@ -30,39 +30,53 @@ cloudinary.config({
 });
 
 module.exports = {
+    // POST to 'Register' page to create new user
     async postRegister (req,res,next) {
         console.log('registering user');
+        //send uploaded image to cloudinary
             await cloudinary.uploader.upload(req.file.path);
+            //set adminStatus to false to initialize
             let adminStatus = false;
                 if(req.body.adminCode === process.env.ADMINCODE) {
+                    //if adminCode provided matches secret, set user's adminStatus to true, giving extra permissions
                     adminStatus = true;
                 }
+                //create a new User object with properties submitted
             const newUser = new User({
                 username:req.body.username,
                 email:req.body.email,
+                //set User's image url and publicId to those returned from Cloudinary
                 'image.url':result.url,
                 'image.publicId':result.public_id,
                 isAdmin:adminStatus});
+                // register new User using these properties
             await User.register(newUser,req.body.password);
             passport.authenticate('local')(req,res,()=> {
                 if(adminStatus === true) {
+                    //if new User is an admin, send this message
                     req.flash('success','Admin Account Created! Welcome to the team, '+req.body.username)
                 }
                 else {
+                    //if new User is not admin, send this message
                     req.flash('success','Account created! Welcome to the club, '+req.body.username+'!');
                 }
+                //redirect to the user's profile after logging in
                 res.redirect('/profile/'+user._id);
             });
     },
-
+    //POST to Login page
     async postLogin (req,res,next) {
+        //use passport to authenticate user
         await passport.authenticate('local',{ 
             successRedirect: '/',
             failureRedirect: '/login' 
-          })(req,res,next);
+          })(req,res,next); //pass these parameters into the passport.authenticate method
     },
+    //GET to Logout route
     async getLogout (req,res,next) {
+        //use passport to log current user out
         await req.logout();
-            res.redirect('/');
+        //redirect to home page once logged out
+        res.redirect('/');
     }
 };
