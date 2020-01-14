e20b38601c3b7a61314c548592deae925020e480
Robert Greenstreet <robertgreenstreet@gmail.com>
2020-01-14 11:27:55 -0600

    add 'ejs-mate' for layout/boilerplate templates into which the body of different view files is inserted

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: '.env' });
 const createError = require('http-errors');
 const express = require('express');
+const engine = require('ejs-mate');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -34,6 +35,8 @@ mongoose.connect(process.env.DATABASE_URL,{
 
 console.log("Environment database URL: "+process.env.DATABASE_URL);
 
+//use ejs-locals for all ejs templates
+app.engine('ejs', engine);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
