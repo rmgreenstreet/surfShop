2b8d6a94c59e2d79f51fd7dda8f4354967d99525
Robert Greenstreet <robertgreenstreet@gmail.com>
2020-03-04 15:23:33 -0600

    restore models

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    image: {
+        url: {
+            type:String,
+            default:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+        },
+        public_id: String
+    },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
+    // ,
+    // isAdmin:{
+    //     type:Boolean,
+    //     default:false
+    // }
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User',userSchema);
